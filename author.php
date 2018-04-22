f2c93abc262e501c6f3fb80f01bb2b9e547eccac
Claire <clairecunliffe@gmail.com>
2018-04-22 21:19:50 +0100

    added author name info, single name for now not FirstName and LastName as per schema, we may want to change that

--- author.php
+++ author.php
@@ -1,8 +1,26 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+class author {
+    
+    public $AuthorName;
+   
+    
+    public function __construct($AuthorName) {
+        $this->AuthorName = $AuthorName;   
+    } 
+    
+    function get_AuthorName() {
+        return $this->AuthorName;
+    }
+}
+
+
+   
+
+    
+    
+    
+ 
+    
 
