183d03cb228cb6569dff2e533aec6ece35d60b11
pallavidughrekar <38434226+pallavidughrekar@users.noreply.github.com>
2018-04-25 21:38:32 +0100

    Second file testing

---
+++ class_books.php
@@ -0,0 +1,85 @@
+<?php
+
+class books{
+    
+    public $title;
+    public $ISBN;
+    public $price;
+    public $year_published;
+    public $current_user;
+    
+    
+   public function __construct1() {
+        /*$this->id = $id;
+        $this->title = $title;
+        $this->ISBN = $ISBN;
+        $this->price = $price;
+        $this->year_published = $year_published;
+        $this->current_user= $current_user;*/
+              
+            
+        
+    } 
+    public function __construct2($id, $title, $ISBN, $price, $year_published, $current_user) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->ISBN = $ISBN;
+        $this->price = $price;
+        $this->year_published = $year_published;
+        $this->current_user= $current_user;
+               
+         
+    } 
+    
+    public function get_id() {
+        return $this->id;
+    }
+
+    public function get_title() {
+        return $this->title;
+    }
+
+    public function get_ISBN() {
+        return $this->ISBN;
+    }
+
+    public function get_price() {
+        return $this->price;
+    }
+
+    public function get_year_published() {
+        return $this->year_published;
+    }
+
+    public function get_current_user() {
+        return $this->current_user;
+    }
+}
+    /*public function set_id($id) {
+        $this->id = $id;
+    }
+
+    function set_title($title) {
+        $this->title = $title;
+    }
+
+    function set_ISBN($ISBN) {
+        $this->ISBN = $ISBN;
+    }
+
+    function set_price($price) {
+        $this->price = $price;
+    }
+
+    function set_year_published($year_published) {
+        $this->year_published = $year_published;
+    }
+
+    function set_current_user($current_user) {
+        $this->current_user = $current_user;
+    }*/
+
+    
+    
+    
+         
